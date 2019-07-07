91ed92c37e787760516800b3a50564a1d81c732f
ahiraaam <ahicruma23@gmail.com>
2019-07-06 22:48:50 -0500

    Add burger menu

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar</div>
-        <main className={classes.Content}>
-            {props.children}     
-        </main>
-    </Aux>
-);
+class Layout extends Component  {
+    state={
+        showSide: true,
+        showSideDrawer:false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSide:false})
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) =>{
+            return {showSide:!prevState.showSideDrawer}
+        }) 
+    }
+    render(){
+        return(
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer closed = {this.sideDrawerClosedHandler} open={this.state.showSide} />
+                <main className={classes.Content}>
+                    {this.props.children}     
+                </main>
+             </Aux>
+        )
+    }
+}
 
-export default layout;
+export default Layout;
+
